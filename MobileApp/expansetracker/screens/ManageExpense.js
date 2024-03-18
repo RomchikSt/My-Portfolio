85f5c47fe656fa6115ae441e13c6d5d0fe1544c9
Romchik Stakhiv <Roma.Stakhiv@gmail.com>
2024-03-18 12:17:11 +0200

    1

---
+++ MobileApp/expansetracker/screens/ManageExpense.js
@@ -0,0 +1,42 @@
+import { Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+import { GlobalStyles } from "../constants/style";
+
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  return <Text>ManageExpense Screen</Text>;
+}
+
+export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
